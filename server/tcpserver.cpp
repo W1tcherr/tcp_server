46f667776ef8c8737c71758dd60411d7934247c8
Alex <frost4tt@gmail.com>
2021-10-17 22:14:56 +0300

    добавлена обработка параметров из консоли, добавлена функция клиенту для получения даты переработана функция read, для чтеня длинных сообщений и для чтения разрыва соединения

--- server/tcpserver.cpp
+++ server/tcpserver.cpp
@@ -12,19 +12,11 @@ void Tcpserver::server_start(int backlog)
             int _client_fd = client_fd;
             int _client_id = client_id++;
             clients.emplace(_client_id, std::thread([this, _client_id, _client_fd]
-                                                   { handle_connection(_client_fd, _client_id); }));
+                                                    { handle_connection(_client_fd, _client_id); }));
         }
-        if (!clients_end.empty())
-        {
-            for (auto &el: clients_end)
-            {
-                clients.find(el)->second.join();
-                clients.erase(el);
-            }
-            clients_end.clear();
-        }
+        cleaning_clients();
         std::cout << "\nmap size: " << clients.size();
-        for(auto &el: clients)
+        for (auto &el: clients)
         {
             std::cout << "\nclients id: " << el.first << "\tthread_id: " << el.second.get_id() << std::endl;
         }
@@ -33,20 +25,38 @@ void Tcpserver::server_start(int backlog)
 
 void Tcpserver::handle_connection(int _client_fd, int _client_id)
 {
-    char* buf = new char[buf_size];
-    nread = Read(_client_fd, buf, sizeof(buf));
-    write(STDOUT_FILENO, buf, nread);
-    write(_client_fd, buf, nread);
+    ssize_t bytes_read = 0;
+    char *buf = new char[buf_size];
+    bool end_connection = true;
+    while (end_connection)
+    {
+        end_connection = Read(_client_fd, buf, buf_size, bytes_read);
+        write(STDOUT_FILENO, buf, bytes_read);
+        write(_client_fd, buf, bytes_read);
+    }
     close(_client_fd);
     delete[] buf;
     //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     clients_end.emplace_back(_client_id);
 }
 
+void Tcpserver::cleaning_clients()
+{
+    if (!clients_end.empty())
+    {
+        for (auto &el: clients_end)
+        {
+            clients.find(el)->second.join();
+            clients.erase(el);
+        }
+        clients_end.clear();
+    }
+}
+
 void Tcpserver::server_stop()
 {
     server_status = Status::down;
-    for(auto& el: clients)
+    for (auto &el: clients)
     {
         el.second.join();
         clients.erase(el.first);
